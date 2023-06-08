fd72196c27d9a9050e9a1a1a24571c8979432297
mengyuan21 <15332392019@163.com>
2023-06-08 21:41:34 +0800

    feat: add elements and css

--- src/practice/flex-UI.js
+++ src/practice/flex-UI.js
@@ -13,20 +13,26 @@ function DemoUI() {
                         <div> Find agencies </div>
                         <div> News </div>
                     </div>
+                    <button> Sign in </button>
+                    <button> Join </button>
                 </div>
                 <div className={"searchBar"}>
-                    <div> Buy </div>
-                    <div> Lease </div>
-                    <div> Sold </div>
-                    <div> Leased </div>
+                    <div className={"searchbarButton"}>
+                        <button> Buy </button>
+                        <button> Lease </button>
+                        <button> Sold </button>
+                        <button> Leased </button>
+                    </div>
+                    <input className={"searchbarInput"} defaultValue={"Where are you looking?"}/>
                 </div>
                 <div className={"propertyType"}>
-                    <div> item 1 </div>
-                    <div> item 2 </div>
-                    <div> item 3 </div>
-                    <div> item 4 </div>
-                    <div> item 5 </div>
-                    <div> item 6 </div>
+                    <div className={"propertyTypeTitle"}> Explore by property type </div>
+                    <div className={"propertyItem"}> item 1 </div>
+                    <div className={"propertyItem"}> item 2 </div>
+                    <div className={"propertyItem"}> item 3 </div>
+                    <div className={"propertyItem"}> item 4 </div>
+                    <div className={"propertyItem"}> item 5 </div>
+                    <div className={"propertyItem"}> item 6 </div>
                 </div>
             </div>
         </div>
