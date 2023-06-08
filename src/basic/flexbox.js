e13128f4bc80a1cc6287807d78cd1eba6cbe9608
mengyuan21 <15332392019@163.com>
2023-06-08 21:05:41 +0800

    feat: add basic demo & css files

---
+++ src/basic/flexbox.js
@@ -0,0 +1,18 @@
+import './flexbox.css';
+
+function Flexbox() {
+  return (
+    <div>
+        <div className={"flexbox-container"}>
+            <div className={"flexbox-item flexbox-item-1"}> Flex item 1 </div>
+            <div className={"flexbox-item flexbox-item-2"}> Flex item 2 </div>
+            <div className={"flexbox-item flexbox-item-3"}> Flex item 3 </div>
+            <div className={"flexbox-item flexbox-item-4"}> Flex item 4 </div>
+            <div className={"flexbox-item flexbox-item-5"}> Flex item 5 </div>
+            <div className={"flexbox-item flexbox-item-6"}> Flex item 6 </div>
+        </div>
+    </div>
+  );
+}
+
+export default Flexbox;
---
+++ src/practice/flex-UI.js
@@ -0,0 +1,15 @@
+import './flex-UI.css';
+
+function Flexbox() {
+    return (
+        <div>
+            <div className={"flexbox-container"}>
+                <div className={'navigation'}> navigation </div>
+                <div className={'searchBar'}> searchBar </div>
+                <div className={''}> searchBar </div>
+            </div>
+        </div>
+    );
+}
+
+export default Flexbox;
