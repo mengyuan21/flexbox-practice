e8e6118703115ead0c107be19fd4b0fcc54c05c4
mengyuan21 <15332392019@163.com>
2023-06-08 21:12:44 +0800

    feat: add demo UI and css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Flexbox from './basic/flexbox';
+import DemoUI from "./practice/flex-UI";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Flexbox />
+    {/*<Flexbox />*/}
+    <DemoUI/>
   </React.StrictMode>
 );
 
--- src/practice/flex-UI.js
+++ src/practice/flex-UI.js
@@ -1,15 +1,26 @@
-import './flex-UI.css';
+import styles from './flex-UI.css';
 
-function Flexbox() {
+function DemoUI() {
     return (
         <div>
-            <div className={"flexbox-container"}>
-                <div className={'navigation'}> navigation </div>
-                <div className={'searchBar'}> searchBar </div>
-                <div className={''}> searchBar </div>
+            <div className={styles.container}>
+                <div className={styles.navigation}>
+                    <div> item 1 </div>
+                    <div> item 2 </div>
+                    <div> item 3 </div>
+                </div>
+                <div className={styles.searchBar}> searchBar </div>
+                <div className={styles.propertyType}>
+                    <div> item 1 </div>
+                    <div> item 2 </div>
+                    <div> item 3 </div>
+                    <div> item 4 </div>
+                    <div> item 5 </div>
+                    <div> item 6 </div>
+                </div>
             </div>
         </div>
     );
 }
 
-export default Flexbox;
+export default DemoUI;
